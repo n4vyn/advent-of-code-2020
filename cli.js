5908bd7c0729c37da73c3f8167a65e338545e6b3
n4vyn <43959137+n4vyn@users.noreply.github.com>
2020-12-05 22:26:30 +0100

    proper error handling with throw & try & catch

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
 // blatantly stolen from Sikari who blatantly stole from https://stackoverflow.com/a/16060619
 const uncachedRequire = module => {
@@ -21,21 +21,29 @@ rl.prompt()
 rl.on('line', (input) => {
 
     let [command, ...args] = input.split(' ')
-    
-    if(commands[command]){
-        commands[command].handler(args)
-    }else{
-        console.log(chalk.hex('#ff0000')('Unrecognized command.'));
+
+    try {
+        if(commands[command]){
+            commands[command].handler(args)
+        }else{
+            console.log(chalk.hex('#ff0000')('Unrecognized command.'));
+        }
+    } catch (error) {
+        console.log(chalk.hex('#ff0000')(error.message))
     }
 
     console.log('');
     rl.prompt()    
 });
 
+function runAndLog(day, task){
+    console.log(chalk.hex('#0000ff')('Result -> ')+chalk.hex('#00ffff')(runDayAndTask(day, task)))
+}
+
 function runDayAndTask(day, task){
     const tasks = uncachedRequire('./day'+day+'/tasks');
-    const data = tasks.prepareData(fs.readFileSync(__dirname+'/day'+day+'/data.txt', "utf-8"));
-    console.log(chalk.hex('#0000ff')('Result -> ')+chalk.hex('#00ffff')(tasks['task'+task](data)))
+    const data = tasks.prepareData(fs.readFileSync('./day'+day+'/data.txt', "utf-8"));
+    return tasks['task'+task](data);
 }
 
 var commands = {
@@ -44,9 +52,8 @@ var commands = {
         handler: (args) => {
             let [day, task] = [...args]
             if(day){
-                if(!fs.existsSync(__dirname+'/day'+day)) {
-                    console.log(chalk.hex('#ff0000')("I haven't solved tasks for this day yet."))
-                    return
+                if(!fs.existsSync('./day'+day)) {
+                    throw new Error("I haven't solved tasks for this day yet.")
                 }else{
                     if(task){
                         if (task == 1 || task == 2){
@@ -65,7 +72,7 @@ var commands = {
                 day = 1;
                 task = 1;
             }
-            runDayAndTask(day, task);
+            runAndLog(day, task);
         }
     },
     help: {
@@ -80,7 +87,7 @@ var commands = {
     quit: {
         expectedArgs: '',
         handler: ()=>{
-            console.log(chalk.hex('#ff0000')('Quitting.'));
+            console.log(chalk.hex('#00ff00')('Quitting.'));
             process.exit()
         }
     },
@@ -89,8 +96,7 @@ var commands = {
         handler: (args)=>{
             const [day] = [...args];
             if(!day){
-                console.log(chalk.hex('#ff0000')('You need to specify day number.'));
-                return;
+                throw new Error('You need to specify day number.')
             }
             if(!fs.existsSync(__dirname+'/day'+day)) {
                 try {
@@ -102,7 +108,7 @@ var commands = {
                     console.log(chalk.hex('#ff0000')(error));
                 }
             }else{
-                console.log(chalk.hex('#ff0000')('Day already exists!'));
+                throw new Error('Day already exists!')
             }
         }
     },
