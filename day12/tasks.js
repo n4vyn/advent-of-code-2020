7c9a54f034bba099a493dc7b2c1152654cab17e2
n4vyn <43959137+n4vyn@users.noreply.github.com>
2020-12-12 16:25:38 +0100

    smol tweak

--- day12/tasks.js
+++ day12/tasks.js
@@ -16,10 +16,9 @@ function task1(data){
         'N': 0
     }
     for (const move of data) {
-        let [type, val] = move.split(/([0-9]+)/i);
-        val = parseInt(val)
+        let [type, val] = move;
         if(type === 'F'){
-            distancesTravelled[directions[(angle%360)/90]]+=val
+            distancesTravelled[directions[(angle%360)/90]] += val
         }else if(type === 'R'){
             angle += val
         }else if(type === 'L'){
@@ -28,16 +27,15 @@ function task1(data){
             distancesTravelled[type]+=val
         }
     }
-    return Math.abs(distancesTravelled.E - distancesTravelled.W) +
-    Math.abs(distancesTravelled.N - distancesTravelled.S)
+    return  Math.abs(distancesTravelled.E - distancesTravelled.W) +
+            Math.abs(distancesTravelled.N - distancesTravelled.S)
 }
 
 function task2(data){
     let wayPoint = [10, 1]
     let position = [0, 0]
     for (const move of data) {
-        let [type, val] = move.split(/([0-9]+)/i);
-        val = parseInt(val)
+        let [type, val] = move;
         if(type === 'E') wayPoint[0] += val
         else if (type === 'W') wayPoint[0] -= val
         else if (type === 'N') wayPoint[1] += val
@@ -48,7 +46,6 @@ function task2(data){
         }
         else if (type === 'L'){
             let nrOfTurns = val/90
-            
             for (let i = 0; i < nrOfTurns; i++) {
                 let p = [...wayPoint]
                 wayPoint[0] = -p[1]
@@ -56,8 +53,7 @@ function task2(data){
             }            
         }
         else if (type === 'R'){
-            let nrOfTurns = val/90
-            
+            let nrOfTurns = val/90            
             for (let i = 0; i < nrOfTurns; i++) {
                 let p = [...wayPoint]
                 wayPoint[0] = p[1]
@@ -65,12 +61,14 @@ function task2(data){
             }
         }
     }
-    console.log(position);
     return Math.abs(position[0])+Math.abs(position[1])
 }
 
 function prepareData(data){
-    return data.trim().split('\r\n');
+    return data.trim().split('\r\n').map(move=>{
+        let [type, val] = move.split(/([0-9]+)/i);
+        return [type, parseInt(val)]
+    });
 }
 
 module.exports = {task1, task2, prepareData};
